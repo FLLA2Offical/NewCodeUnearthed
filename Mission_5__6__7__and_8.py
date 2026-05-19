131ee1aa2c92be4d26cec5ff965e256a58dca537
FLLA2Offical <fll.69309@gmail.com>
2026-05-19 17:19:31 -0400

    Adjust straight distance and LAM.run_time calls
    Updated the straight and LAM.run_time parameters for better control.

--- Mission_5__6__7__and_8.py
+++ Mission_5__6__7__and_8.py
@@ -20,7 +20,7 @@ turn(25)
 straight(350)
 
 RAM.run_time(-1500, 1000, Stop.HOLD) #make the drop the arm 
-straight(50)
+straight(70)
 RAM.run_time(1500, 1000, Stop.HOLD) #make the drop the arm 
 
 straight(-15)
@@ -33,10 +33,14 @@ turn_to(-90)   # reads current heading, turns LEFT to reach 179°
 
 
 straight(-172)
-LAM.run_time(-1500, 1000) #make the drop the arm 
-LAM.run_time(1500, 1000) #make the arm go up
-LAM.run_time(-1500, 1000) #make the arm go down
-LAM.run_time(1500, 1000) #make the arm go up
+LAM.run_time(-1500, 1000, Stop.NONE) #make the drop the arm 
+LAM.run_time(1500, 1000, Stop.NONE) #make the arm go up
+LAM.run_time(-1500, 1000, Stop.NONE) #make the arm go down
+LAM.run_time(1500, 1000, Stop.NONE) #make the arm go up
+LAM.run_time(-1500, 1000, Stop.NONE) #make the drop the arm 
+LAM.run_time(1500, 1000, Stop.NONE) #make the arm go up
+LAM.run_time(-1500, 1000, Stop.NONE) #make the arm go down
+LAM.run_time(1500, 1000, Stop.NONE) #make the arm go up
 
 arc(-250, angle_deg=142 )
 straight(300)
